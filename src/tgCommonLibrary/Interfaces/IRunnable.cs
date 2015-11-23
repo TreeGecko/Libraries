9719b24f072f0f05c5008b3095ffffeb27c3be24
Brian Nelson <brian@treegecko.com>
2015-11-22 21:28:40 -0700

    Add a IRunnable that is slightly different in purpose from IWorker
    IRunnable doesn't have a concept of done.

---
+++ src/tgCommonLibrary/Interfaces/IRunnable.cs
@@ -0,0 +1,12 @@
+﻿namespace TreeGecko.Library.Common.Interfaces
+{
+    /// <summary>
+    /// Similar to the IWorker, but an IRunnable doesn't 
+    /// have the same concept of work completion.
+    /// </summary>
+    public interface IRunnable
+    {
+        void Start();
+        void Stop();
+    }
+}
