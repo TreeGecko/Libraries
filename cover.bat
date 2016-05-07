0d1bfcc7c894001ee4351b3fd8cf829a41b86325
Brian Nelson <brian@treegecko.com>
2016-05-07 12:35:24 -0700

    fix filter

--- cover.bat
+++ cover.bat
@@ -1,6 +1,6 @@
 c:\projects\libraries\src\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe ^
 	-register:user ^
 	-output:c:\projects\libraries\tglibraries-coverage.xml ^
-	"-filter:+[tg]*  -[tgAWS]TreeGecko.Library.AWS.Properties.*;[tgCommonLibrary]TreeGecko.Library.Common.Properties.*;[tgGeospatial]TreeGecko.Library.Geospatial.Properties.*;[tgLoggly]TreeGecko.Library.Loggly.Properties.*;[tgMongo]TreeGecko.Library.Mongo.Properties.*;[tgNet]TreeGecko.Library.Net.Properties.*" ^
+	"-filter:+[tg*]*  -[tg*]*Properties.*" ^
 	-excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" ^
 	"-target:c:\projects\libraries\test.bat"
