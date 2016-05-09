fd9f5a0b0c8014f47f255bea2f955526b8a6ca2b
Brian Nelson <brian@treegecko.com>
2016-05-09 05:58:10 -0700

    fixed test dll name

--- test.bat
+++ test.bat
@@ -1,3 +1,3 @@
 c:\projects\libraries\src\packages\NUnit.ConsoleRunner.3.2.1\tools\nunit3-console.exe  ^
 	--result=c:\projects\results-test.xml;format=nunit2  ^
-	"c:\projects\libraries\src\tgTests\bin\x64\Debug\tgTests.dll"
+	"c:\projects\libraries\src\tgTests\bin\x64\Debug\TreeGeckoTests.dll"
