7e3f039f0525c17c519ba1f8d82c11d38a065cd5
Brian Nelson <brian@treegecko.com>
2016-05-14 21:22:22 -0700

    test

---
+++ src/tgTests/Common/Helpers/GuidHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using TreeGecko.Library.Common.Helpers;
+
+namespace tgTests.Common.Helpers
+{
+    [TestFixture]
+    public class GuidHelperTests
+    {
+        [Test]
+        public void TestOne()
+        {
+            string temp = Guid.NewGuid().ToString();
+
+            bool result = GuidHelper.IsValidGuidString(temp);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void TestTwo()
+        {
+            string temp = "test";
+
+            bool result = GuidHelper.IsValidGuidString(temp);
+            Assert.IsFalse(result);
+        }
+    }
+}
