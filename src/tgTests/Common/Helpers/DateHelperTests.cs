03cf7b6f1b9ae4cf91c9bf4586fa5704f7060dda
Brian Nelson <brian@treegecko.com>
2016-05-14 20:43:14 -0700

    Date Tests

--- src/tgTests/Common/Helpers/DateHelperTests.cs
+++ src/tgTests/Common/Helpers/DateHelperTests.cs
@@ -19,6 +19,29 @@ namespace tgTests.Common.Helpers
         }
 
         [Test]
+        public void ParseDateTimeNull()
+        {
+            try
+            {
+                DateTime outputDateTime = DateHelper.ParseDateTimeString(null);
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<ArgumentNullException>(ex);
+            }
+        }
+
+        [Test]
+        public void ParseString()
+        {
+            DateTime time = DateTime.UtcNow;
+
+            DateTime? value = DateHelper.ParseDate(time.ToShortDateString());
+
+            Assert.IsNotNull(value);
+        }
+
+        [Test]
         public void ParseDateTimeUFormat()
         {
             DateTime now = new DateTime();
